9d1629f38a9c99f33eafea297284edef14500ba2
Athuos <lurquia24@gmail.com>
2022-03-23 22:16:21 -0600

    Proyecto limpio y preconfigurado

---
+++ ToDoAPI/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UsersController : ControllerBase
+    {
+        
+
+    }
+}
